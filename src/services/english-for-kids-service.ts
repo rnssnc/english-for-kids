bca3904363c325fd31bfec4c61e6c2f447a7e8a6
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-07-06 23:21:54 +0300

    feat: add services

---
+++ src/services/english-for-kids-service.ts
@@ -0,0 +1,48 @@
+export type TCategory = {
+  id: number;
+  title: string;
+  imgSrc: string;
+  cardCount: number;
+};
+
+export type TCard = {
+  word: string;
+  translation: string;
+  imageSrc: string;
+  audioSrc: string;
+  isGuessed: boolean;
+};
+
+export type TGameAssets = {
+  correctSoundSrc: string;
+  incorrecSoundtSrc: string;
+  winSoundSrc: string;
+  looseSoundSrc: string;
+  winImageSrc: string;
+  looseImageSrc: string;
+};
+
+export default class EnglishForKidsService {
+  async getCategories() {
+    return (await fetch('./assets/categories.json')).json();
+  }
+
+  async getCategoryCards(id: number) {
+    return new Promise((resolve, reject) => {
+      setTimeout(async () => {
+        resolve((await fetch(`./assets/categories/${id}.json`)).json());
+      }, 700);
+    });
+    return (await fetch(`./assets/categories/${id}.json`)).json();
+  }
+
+  async getGameAssets() {
+    return (await fetch(`./assets/assets.json`)).json();
+  }
+
+  async getCategoryCard(categoryId: number, cardWord: string) {
+    const cards = await (await fetch(`./assets/categories/${categoryId}.json`)).json();
+
+    return cards.find((card: TCard) => card.word === cardWord);
+  }
+}
---
+++ src/services/statistic-service.ts
@@ -0,0 +1,81 @@
+import { TCard } from './english-for-kids-service';
+
+export type TWordStatistic = {
+  card: TCard;
+  category: string;
+  trainCount: number;
+  correctCount: number;
+  incorrectCount: number;
+  precent: string;
+};
+
+export default class StatisticService {
+  static keyName = 'wordStatistic';
+
+  static getStatistic(): TWordStatistic[] {
+    const data = localStorage.getItem(this.keyName);
+    if (data) return JSON.parse(data);
+
+    return [];
+  }
+
+  static clearStatistic() {
+    localStorage.setItem(this.keyName, '[]');
+  }
+
+  static increaseWordCorrect(card: TCard, category: string) {
+    this.increaseField(card, category, 'correctCount');
+  }
+
+  static increaseWordIncorrect(card: TCard, category: string) {
+    this.increaseField(card, category, 'incorrectCount');
+  }
+
+  static increaseWordTrain(card: TCard, category: string) {
+    this.increaseField(card, category, 'trainCount', false);
+  }
+
+  private static increaseField(
+    card: TCard,
+    category: string,
+    field: keyof TWordStatistic,
+    recalculatePrecents = true,
+  ) {
+    const statistic = this.getStatistic();
+
+    const index = statistic.findIndex(
+      (item) => item.card.word === card.word && item.category === category,
+    );
+
+    if (index > -1) {
+      statistic[index][field]++;
+
+      if (recalculatePrecents)
+        statistic[index].precent = `${
+          (statistic[index].correctCount /
+            (statistic[index].incorrectCount + statistic[index].correctCount)) *
+          100
+        }%`;
+
+      localStorage.setItem(this.keyName, JSON.stringify(statistic));
+      return;
+    }
+
+    this.pushNewItem(card, category);
+  }
+
+  private static pushNewItem(card: TCard, category: string) {
+    const statistic = this.getStatistic();
+
+    statistic.push({
+      card: card,
+      category,
+      trainCount: 0,
+      correctCount: 0,
+      incorrectCount: 0,
+      precent: '-',
+    });
+
+    localStorage.setItem(this.keyName, JSON.stringify(statistic));
+  }
+}
